9b63bbb2fec5232ec408d3cb3738a61aea55ec6f
MariaSvintsova <m.svintsova@it-park.tech>
2024-05-05 16:44:29 +0300

    25_2 with tests

--- materials/tests.py
+++ materials/tests.py
@@ -9,7 +9,7 @@ from materials.models import Course, Lesson
 from users.models import User
 
 
-class MaterialsCRUDTestCase(APITestCase):
+class LessondCRUDTestCase(APITestCase):
 
     def setUp(self) -> None:
         self.user = User.objects.create_user(username='admin', email='admin@gmail.com', password='12345')
@@ -17,23 +17,6 @@ class MaterialsCRUDTestCase(APITestCase):
         self.client.force_authenticate(user=self.user)
         self.lesson = Lesson.objects.create(title='Test Lesson', course=self.course, owner=self.user)
         self.client.force_authenticate(user=self.user)
-
-
-    def test_course_retrieve(self):
-
-        """ Получение курса - выполянется """
-
-        url = reverse("materials:course-detail", args=(self.course.pk,))
-        response = self.client.get(url)
-        data = response.json()
-        self.assertEqual(
-            response.status_code, status.HTTP_200_OK
-        )
-
-        self.assertEqual(
-            data.get('title'), self.course.title
-        )
-
 
     # def test_lesson_create(self):
     #     url = reverse("materials:lesson-create")
@@ -77,19 +60,66 @@ class MaterialsCRUDTestCase(APITestCase):
             data.get('description'), self.lesson.description
         )
 
-    def test_lesson_update(self):
-        url = reverse("materials:lesson-update", args=(self.lesson.pk,))
-        data = {
-            'title': 'Russian lessons',
-            'description': 'Russian with confidence',
-            'video_link': 'https://www.youtube.com/watch?v=2To6EqvVPYo',
-            'course': self.course.id
-        }
-        response = self.client.patch(url, data)
+    # def test_lesson_update(self):
+    #     url = reverse("materials:lesson-update", args=(self.lesson.pk,))
+    #     data = {
+    #         'title': 'Russian lessons',
+    #         'description': 'Russian with confidence',
+    #         'video_link': 'https://www.youtube.com/watch?v=2To6EqvVPYo',
+    #         'course': self.course.id
+    #     }
+    #     response = self.client.patch(url, data)
+    #     data = response.json()
+    #     self.assertEqual(
+    #         response.status_code, status.HTTP_200_OK
+    #     )
+    #     self.assertEqual(
+    #         data.get('title'), "Russian lessons"
+    #     )
+
+    def test_lesson_delete(self):
+
+        """ Удаление урока - выполянется """
+
+        url = reverse("materials:lesson-delete", args=(self.lesson.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT
+        )
+        self.assertEqual(
+                Lesson.objects.all().count(), 0
+        )
+
+    def test_lesson_list(self):
+
+        """ Получение списка уроков - выполянется """
+
+        url = reverse("materials:lesson-list")
+        response = self.client.get(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+
+
+class CourseTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username='admin', email='admin@gmail.com', password='12345')
+        self.course = Course.objects.create(title='Test Course', description='This is a test course')
+        self.client.force_authenticate(user=self.user)
+        self.lesson = Lesson.objects.create(title='Test Lesson', course=self.course, owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_course_retrieve(self):
+        """ Получение курса - выполянется """
+
+        url = reverse("materials:course-detail", args=(self.course.pk,))
+        response = self.client.get(url)
         data = response.json()
         self.assertEqual(
             response.status_code, status.HTTP_200_OK
         )
+
         self.assertEqual(
-            data.get('title'), "Russian lessons"
+            data.get('title'), self.course.title
         )
