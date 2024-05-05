83a9c2f07b37c9f06c5e70308be93a936ea9a64a
MariaSvintsova <m.svintsova@it-park.tech>
2024-05-05 16:50:30 +0300

    25_2 with tests

--- materials/tests.py
+++ materials/tests.py
@@ -18,26 +18,26 @@ class LessondCRUDTestCase(APITestCase):
         self.lesson = Lesson.objects.create(title='Test Lesson', course=self.course, owner=self.user)
         self.client.force_authenticate(user=self.user)
 
-    # def test_lesson_create(self):
-    #     url = reverse("materials:lesson-create")
-    #     data = {
-    #         'title': 'English lessons',
-    #         'description': 'English with confidence',
-    #         'video_link': 'https://www.youtube.com/watch?v=2To6EqvVPYo',
-    #         'course': self.course.id
-    #     }
-    #     response = self.client.post(url, data, content_type='application/json')
-    #     print(response.content)  # Выводим содержимое ответа
-    #     print(response.status_code)
-    #     self.assertEqual(
-    #         response.status_code, status.HTTP_201_CREATED
-    #     )
-    #     self.assertTrue(
-    #         Lesson.objects.filter(title='English lessons').exists()
-    #     )
-    #     self.assertEqual(
-    #         Lesson.objects.all().count(), 1
-    #     )
+    def test_lesson_create(self):
+        url = reverse("materials:lesson-create")
+        data = {
+            'title': 'English lessons',
+            'description': 'English with confidence',
+            'video_link': 'https://www.youtube.com/watch?v=2To6EqvVPYo',
+            'course': self.course.id
+        }
+        response = self.client.post(url, data, content_type='application/json')
+        print(response.json())  # Выводим содержимое ответа
+        print(response.status_code)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertTrue(
+            Lesson.objects.filter(title='English lessons').exists()
+        )
+        self.assertEqual(
+            Lesson.objects.all().count(), 1
+        )
 
 
     def test_lesson_retrieve(self):
