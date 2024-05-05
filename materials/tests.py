1249a0e6ce8556e24c1f83ec73e96e1d2a9731bb
MariaSvintsova <m.svintsova@it-park.tech>
2024-05-05 15:56:47 +0300

    25_2

--- materials/tests.py
+++ materials/tests.py
@@ -20,6 +20,9 @@ class MaterialsCRUDTestCase(APITestCase):
 
 
     def test_course_retrieve(self):
+
+        """ Получение курса - выполянется """
+
         url = reverse("materials:course-detail", args=(self.course.pk,))
         response = self.client.get(url)
         data = response.json()
@@ -32,18 +35,61 @@ class MaterialsCRUDTestCase(APITestCase):
         )
 
 
-    def test_lesson_create(self):
-        url = reverse("materials:lesson-create")
+    # def test_lesson_create(self):
+    #     url = reverse("materials:lesson-create")
+    #     data = {
+    #         'title': 'English lessons',
+    #         'description': 'English with confidence',
+    #         'video_link': 'https://www.youtube.com/watch?v=2To6EqvVPYo',
+    #         'course': self.course.id
+    #     }
+    #     response = self.client.post(url, data, content_type='application/json')
+    #     print(response.content)  # Выводим содержимое ответа
+    #     print(response.status_code)
+    #     self.assertEqual(
+    #         response.status_code, status.HTTP_201_CREATED
+    #     )
+    #     self.assertTrue(
+    #         Lesson.objects.filter(title='English lessons').exists()
+    #     )
+    #     self.assertEqual(
+    #         Lesson.objects.all().count(), 1
+    #     )
+
+
+    def test_lesson_retrieve(self):
+
+        """ Получение урока - выполянется """
+
+        url = reverse("materials:lesson-get", args=(self.lesson.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get('title'), self.lesson.title
+        )
+        self.assertEqual(
+            data.get('id'), self.lesson.pk
+        )
+        self.assertEqual(
+            data.get('description'), self.lesson.description
+        )
+
+    def test_lesson_update(self):
+        url = reverse("materials:lesson-update", args=(self.lesson.pk,))
         data = {
-            'title': 'English lessons',
-            'description': 'English with confidence',
+            'title': 'Russian lessons',
+            'description': 'Russian with confidence',
             'video_link': 'https://www.youtube.com/watch?v=2To6EqvVPYo',
             'course': self.course.id
         }
-        response = self.client.post(url, data, content_type='application/json')
-        print(response.content)  # Выводим содержимое ответа
-        print(response.status_code)
+        response = self.client.patch(url, data)
+        data = response.json()
         self.assertEqual(
-            response.status_code, status.HTTP_201_CREATED
+            response.status_code, status.HTTP_200_OK
         )
-        self.assertTrue(Lesson.objects.filter(title='English lessons').exists())
+        self.assertEqual(
+            data.get('title'), "Russian lessons"
+        )
