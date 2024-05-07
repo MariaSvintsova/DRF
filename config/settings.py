eaded556cdc868967c2046b488e2ed562e1c6563
MariaSvintsova <m.svintsova@it-park.tech>
2024-05-07 18:26:33 +0300

    25_2 with env

--- config/settings.py
+++ config/settings.py
@@ -11,19 +11,23 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^%0w)_3z)sz-kwkg8q=qlaljmy=^bl*xso0lrv&^*6!8^27jyd'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -96,12 +100,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'drfhw',
-        'USER': 'postgres',
-        'HOST': '127.0.0.1',
-        'PASSWORD': 'Msssvintsova006',
-        'PORT': 5432
+        'ENGINE': os.environ.get("DATABASE_ENGINE"),
+        'NAME': os.environ.get("DATABASE_USER"),
+        'USER': os.environ.get("DATABASE_USER"),
+        'HOST': os.environ.get("DATABASE_HOST"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'PORT': os.environ.get("DATABASE_PORT")
     }
 }
 
